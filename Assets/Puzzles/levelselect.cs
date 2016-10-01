19fcbec081442524aebcfcdcae42b6f0506e650c
Ian_Mihuro <nickmihuro@gmail.com>
2016-10-02 02:10:58 +0300

    Main Menu Done

--- Assets/Puzzles/levelselect.cs
+++ Assets/Puzzles/levelselect.cs
@@ -11,9 +11,13 @@ public class levelselect : MonoBehaviour {
 
     public static int whichLevel;
 
+    private AudioSource acceptSound;
+
+
     // Use this for initialization
     void Start()
     {
+        acceptSound = GetComponentInParent<AudioSource>();
 
     }
 
@@ -25,15 +29,27 @@ public class levelselect : MonoBehaviour {
 
     void OnMouseDown()
     {
-        //Random rnd = new Random();
-        //int rndLevel = Random.Range(1, 2);
+        acceptSound.Play();
+        StartCoroutine(Wait(gameObject));    
+        
+ 
         Debug.Log(gameObject.name);
+
+
+        
+    }
+
+    IEnumerator Wait(GameObject gameObject)
+    {
+        yield return new WaitForSeconds(3);
+
 
         if (gameObject.name == "Ending")
         {
             whichLevel = 1; //This id for testing purposes. Comment when done
-            SceneManager.LoadScene("puzzle_1");
             
+            SceneManager.LoadScene("puzzle_1");
+
 
         }
 
@@ -41,26 +57,26 @@ public class levelselect : MonoBehaviour {
         if (gameObject.name == "Prologue")
         {
             whichLevel = 2; //This id for testing purposes. Comment when done
-            //SceneManager.LoadScene("Level1");
+            
+            SceneManager.LoadScene("Level1");
 
         }
 
         if (gameObject.name == "The Town")
         {
             whichLevel = 3; //This id for testing purposes. Comment when done
-                            //SceneManager.LoadScene("Level2");
+            
+            //SceneManager.LoadScene("Level2");
 
         }
 
         if (gameObject.name == "The Forest")
         {
             whichLevel = 4; //This id for testing purposes. Comment when done
+            
             //SceneManager.LoadScene("Level3");
 
         }
-
-        //SceneManager.LoadScene("puzzle_1");
-
 
     }
 }
